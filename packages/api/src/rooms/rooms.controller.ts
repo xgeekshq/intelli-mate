da930665152d35d1148645e19c32f80138d5bf81
comoser <david.alecrim1@gmail.com>
2023-06-16 09:45:00 +0100

    feat: fix dto in swagger

--- packages/api/src/rooms/rooms.controller.ts
+++ packages/api/src/rooms/rooms.controller.ts
@@ -1,5 +1,6 @@
 import { ClerkAuthGuard } from '@/auth/guards/clerk/clerk.auth.guard';
 import { ApiClerkAuthHeaders } from '@/auth/guards/clerk/open-api-clerk-headers.decorator';
+import { CreateRoomRequestDto } from '@/rooms/dtos/create-room.request.dto';
 import { RoomResponseDto } from '@/rooms/dtos/room.response.dto';
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
@@ -22,7 +23,7 @@ export class RoomsController {
   @UseGuards(ClerkAuthGuard)
   createRoom(
     @Request() req: Request,
-    @Body() createRoomRequestDto: RoomResponseDto
+    @Body() createRoomRequestDto: CreateRoomRequestDto
   ): Promise<RoomResponseDto> {
     return this.createRoomUsecase.execute(
       req.auth.userId,
