e83f9524629c764beb0cc2f4f39047f0e40f1ede
comoser <david.alecrim1@gmail.com>
2023-06-20 15:37:36 +0100

    feat: remove unused imports

--- packages/api/src/rooms/usecases/update-room-settings.usecase.ts
+++ packages/api/src/rooms/usecases/update-room-settings.usecase.ts
@@ -1,14 +1,10 @@
-import { ClerkAuthUserProvider } from '@/auth/providers/clerk/clerk-auth-user.provider';
 import { InternalServerErrorException } from '@/common/exceptions/internal-server-error.exception';
-import { UserNotFoundException } from '@/common/exceptions/user-not-found.exception';
 import { Usecase } from '@/common/types/usecase';
 import { RoomResponseSchema } from '@/contract/rooms/room.response.dto';
-import { InviteUserToRoomRequestDto } from '@/rooms/dtos/invite-user-to-room.request.dto';
 import { RoomResponseDto } from '@/rooms/dtos/room.response.dto';
 import { UpdateRoomSettingsRequestDto } from '@/rooms/dtos/update-room-settings.request.dto';
 import { NoRoomSettingsDefinedException } from '@/rooms/exceptions/no-room-settings-defined.exception';
 import { NotRoomOwnerException } from '@/rooms/exceptions/not-room-owner.exception';
-import { UserAlreadyInRoomException } from '@/rooms/exceptions/user-already-in-room.exception';
 import { RoomsRepository } from '@/rooms/rooms.repository';
 import { Injectable } from '@nestjs/common';
 
