449b75d4341967ff5661c3ec6df7a745a79cd0af
romansharapov19 <r.sharapov@kigroup.de>
2023-06-20 16:54:04 +0100

    feat: create room update section

--- packages/web-ui/components/ui/button.tsx
+++ packages/web-ui/components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'underline-offset-4 hover:underline text-primary',
+        success: 'bg-green-500 text-primary-foreground hover:bg-green-500/80',
       },
       size: {
         default: 'h-10 py-2 px-4',
