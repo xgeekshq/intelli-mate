3e7f979c5c39aed970ced7cdcd60f5fd1ee540d8
romansharapov19 <r.sharapov@kigroup.de>
2023-06-29 14:07:30 +0100

    fix: clean code

--- packages/web-ui/app/rooms/[room]/page.tsx
+++ packages/web-ui/app/rooms/[room]/page.tsx
@@ -1,6 +1,6 @@
 import Chat from '@/components/chat';
 
-export default async function Room({ params }: { params: { room: string } }) {
+export default function Room({ params }: { params: { room: string } }) {
   return (
     <div className="flex h-full flex-col">
       <Chat roomId={params.room}></Chat>
