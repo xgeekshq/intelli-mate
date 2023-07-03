04947ef08b13762f916592a1594a09c8d2eac2b0
romansharapov19 <r.sharapov@kigroup.de>
2023-07-03 17:55:50 +0100

    feat: create an external config file for api

--- packages/api/src/ai/services/ai.service.ts
+++ packages/api/src/ai/services/ai.service.ts
@@ -1,13 +1,19 @@
 import { ChainService } from '@/ai/services/chain.service';
 import { Injectable } from '@nestjs/common';
+import * as config from 'config';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const config = require('config');
 
 @Injectable()
 export class AiService {
   llmModel: ChatOpenAI;
 
   constructor(private readonly chainService: ChainService) {
-    this.llmModel = new ChatOpenAI({ temperature: 0.2 });
+    this.llmModel = new ChatOpenAI({
+      temperature: config.get('ai.default-temperature'),
+    });
   }
 
   async askAiInFreeText(input: string, chatId: string) {
