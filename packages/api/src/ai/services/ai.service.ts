25beda56baa757b0bcb68b4fca4098303ff0023f
romansharapov19 <r.sharapov@kigroup.de>
2023-07-06 17:44:47 +0100

    feat: remove comments

--- packages/api/src/ai/services/ai.service.ts
+++ packages/api/src/ai/services/ai.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from '@nestjs/common';
 import * as config from 'config';
 import { ChatOpenAI } from 'langchain/chat_models/openai';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-// const config = require('config');
-
 @Injectable()
 export class AiService {
   llmModel: ChatOpenAI;
