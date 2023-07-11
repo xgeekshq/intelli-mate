176870c0b69bfc4ea0bc38ff9863339f5a8a5ed9
tiagomichaelsousa <tiagomichaelsousa@gmail.com>
2023-07-11 11:07:40 +0100

    fix(connections): update redis and mongo connections

--- packages/api/src/database/database.providers.ts
+++ packages/api/src/database/database.providers.ts
@@ -5,8 +5,17 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION_KEY,
-    useFactory: (configService: ConfigService): Promise<typeof mongoose> =>
-      mongoose.connect(configService.get('MONGO_DB_CONNECTION_URL')),
+    useFactory: (configService: ConfigService): Promise<typeof mongoose> => {
+      const connection = configService.get('MONGO_DB_CONNECTION_URL');
+      const dbName = configService.get('MONGO_DB_NAME');
+      const lastIndex = connection.lastIndexOf('/');
+      const url = `${connection.substring(
+        0,
+        lastIndex + 1
+      )}${dbName}${connection.substring(lastIndex + 1)}`;
+
+      return mongoose.connect(url);
+    },
     inject: [ConfigService],
   },
 ];
