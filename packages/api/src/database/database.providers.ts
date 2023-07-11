509f4485412097319bf2c23fdd23daf9e8746e61
tiagomichaelsousa <tiagomichaelsousa@gmail.com>
2023-07-11 12:54:09 +0100

    fix(db): add mongo database name

--- packages/api/src/database/database.providers.ts
+++ packages/api/src/database/database.providers.ts
@@ -5,8 +5,17 @@ import * as mongoose from 'mongoose';
 export const databaseProviders = [
   {
     provide: DATABASE_CONNECTION_KEY,
-    useFactory: (configService: ConfigService): Promise<typeof mongoose> =>
-      mongoose.connect(configService.get('MONGO_DB_CONNECTION_URL')),
+    useFactory: (configService: ConfigService): Promise<typeof mongoose> => {
+      const connection = configService.get('MONGO_DB_CONNECTION_URL');
+      const dbName = configService.get('MONGO_DB_NAME');
+      const index = connection.lastIndexOf('/');
+      const url = `${connection.substring(
+        0,
+        index + 1
+      )}${dbName}${connection.substring(index + 1)}`;
+
+      return mongoose.connect(url);
+    },
     inject: [ConfigService],
   },
 ];
