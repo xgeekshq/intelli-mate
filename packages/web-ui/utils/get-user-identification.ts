6bd231b815a455005db92fb2a10bb0b244f09cd5
Joana Santos <j.santos@kigroup.de>
2023-07-14 18:04:53 +0100

    fix: inconsistency in profile data

--- packages/web-ui/utils/get-user-identification.ts
+++ packages/web-ui/utils/get-user-identification.ts
@@ -1,11 +1,5 @@
 import { ChatUserType } from '@/types/chat';
 
 export const getUserIdentification = (user: ChatUserType): string => {
-  if (user.name) {
-    return user.name;
-  }
-  if (user.userName) {
-    return user.userName;
-  }
-  return user.email;
+  return user.name ?? user.userName ?? user.email;
 };
