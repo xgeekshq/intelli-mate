15f8ab87f8bfefef1c9ffeb88777c3fddd2e82be
romansharapov19 <r.sharapov@kigroup.de>
2023-07-25 13:04:58 +0100

    feat: add scroll to admin user list

--- packages/web-ui/app/admin/users/page.tsx
+++ packages/web-ui/app/admin/users/page.tsx
@@ -8,6 +8,7 @@ import { alphaAscSortPredicate } from '@/utils/sort-predicates';
 
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
 import { Card } from '@/components/ui/card';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import {
   Table,
   TableBody,
@@ -50,46 +51,51 @@ export default async function AdminUsers() {
   return (
     <div className="flex h-full w-full p-4">
       <Card className="w-full">
-        <Table>
-          <TableCaption>
-            All intelli-mate users are listed here along with their roles.
-          </TableCaption>
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-[80px]"></TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Email</TableHead>
-              <TableHead>Roles</TableHead>
-              <TableHead className="text-right"></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {users.map((user) => (
-              <TableRow key={user.id}>
-                <TableCell>
-                  <Avatar className="h-8 w-8 text-right">
-                    <AvatarImage
-                      src={user.profileImageUrl}
-                      alt="Profile Image"
-                    />
-                  </Avatar>
-                </TableCell>
-                <TableCell>
-                  {user.firstName && user.lastName
-                    ? `${user.firstName} ${user.lastName}`
-                    : 'N/A'}
-                </TableCell>
-                <TableCell>{getUserEmail(user)}</TableCell>
-                <TableCell>
-                  {user.roles.sort(alphaAscSortPredicate).join(', ')}
-                </TableCell>
-                <TableCell className="text-right">
-                  <UpdateRolesForm userId={user.id} defaultRoles={user.roles} />
-                </TableCell>
+        <ScrollArea className="h-full">
+          <Table>
+            <TableCaption>
+              All intelli-mate users are listed here along with their roles.
+            </TableCaption>
+            <TableHeader className="bg-background sticky top-0">
+              <TableRow>
+                <TableHead className="w-20"></TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Email</TableHead>
+                <TableHead>Roles</TableHead>
+                <TableHead className="text-right"></TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+            </TableHeader>
+            <TableBody>
+              {users.map((user) => (
+                <TableRow key={user.id}>
+                  <TableCell>
+                    <Avatar className="h-8 w-8 text-right">
+                      <AvatarImage
+                        src={user.profileImageUrl}
+                        alt="Profile Image"
+                      />
+                    </Avatar>
+                  </TableCell>
+                  <TableCell>
+                    {user.firstName && user.lastName
+                      ? `${user.firstName} ${user.lastName}`
+                      : 'N/A'}
+                  </TableCell>
+                  <TableCell>{getUserEmail(user)}</TableCell>
+                  <TableCell>
+                    {user.roles.sort(alphaAscSortPredicate).join(', ')}
+                  </TableCell>
+                  <TableCell className="text-right">
+                    <UpdateRolesForm
+                      userId={user.id}
+                      defaultRoles={user.roles}
+                    />
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </ScrollArea>
       </Card>
     </div>
   );
--- packages/web-ui/components/ui/avatar.tsx
+++ packages/web-ui/components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      'relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full',
+      'flex h-10 w-10 shrink-0 overflow-hidden rounded-full',
       className
     )}
     {...props}
@@ -39,7 +39,7 @@ const AvatarFallback = React.forwardRef<
   <AvatarPrimitive.Fallback
     ref={ref}
     className={cn(
-      'flex h-full w-full items-center justify-center rounded-full bg-muted',
+      'bg-muted flex h-full w-full items-center justify-center rounded-full',
       className
     )}
     {...props}
