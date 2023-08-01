76d9df06b1e3e18d6f0e05e4d52b8ad6313871d0
romansharapov19 <r.sharapov@kigroup.de>
2023-08-01 19:42:13 +0100

    feat:add redisIoAdapter

--- packages/web-ui/app/state/socket.ts
+++ packages/web-ui/app/state/socket.ts
@@ -2,7 +2,9 @@ import { atom } from 'recoil';
 import io, { Socket } from 'socket.io-client';
 
 // @ts-ignore
-const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL);
+const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
+  withCredentials: true,
+});
 
 export const socketState = atom<Socket>({
   key: 'Socket',
