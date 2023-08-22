521d4e7608b8efb69d0049ae3758e9780a9dc28f
comoser <david.alecrim1@gmail.com>
2023-08-22 15:48:54 +0100

    refactor: url base

--- packages/web-ui/components/main-nav.tsx
+++ packages/web-ui/components/main-nav.tsx
@@ -13,7 +13,7 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="flex items-center space-x-2">
+      <Link href="/rooms" className="flex items-center space-x-2">
         <Icons.logo className="h-6 w-6" />
         <span className="inline-block font-bold">{siteConfig.name}</span>
       </Link>
