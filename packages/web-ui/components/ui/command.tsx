f5a97dca625bd2efdb325802f8efce5beab4326d
comoser <david.alecrim1@gmail.com>
2023-08-22 15:39:13 +0100

    feat: fix font size

--- packages/web-ui/components/ui/command.tsx
+++ packages/web-ui/components/ui/command.tsx
@@ -85,7 +85,7 @@ const CommandGroup = React.forwardRef<
   <CommandPrimitive.Group
     ref={ref}
     className={cn(
-      '[&_[cmdk-group-heading]]:text-xs overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground',
+      'overflow-hidden p-1 text-foreground [&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:py-1.5 [&_[cmdk-group-heading]]:text-xs [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground',
       className
     )}
     {...props}
@@ -129,7 +129,7 @@ const CommandShortcut = ({
   return (
     <span
       className={cn(
-        'text-xs ml-auto tracking-widest text-muted-foreground',
+        'ml-auto text-xs tracking-widest text-muted-foreground',
         className
       )}
       {...props}
