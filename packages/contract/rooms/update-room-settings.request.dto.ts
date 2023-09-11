a3035c5ffacf5d725a9b7d4c137cd0890bd7cd7b
romansharapov19 <95644495+romansharapov19@users.noreply.github.com>
2023-09-11 10:19:32 +0100

    fix: protect the room name update to empty spaces (#176)
    fix: protect room name update to spaces

--- packages/contract/rooms/update-room-settings.request.dto.ts
+++ packages/contract/rooms/update-room-settings.request.dto.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 
 export const UpdateRoomSettingsRequestSchema = z.object({
-  name: z.string().optional(),
+  name: z
+    .string()
+    .transform((value) => value.trim())
+    .refine((value) => value.length >= 3, {
+      message: "Name must have at least 3 characters",
+    })
+    .refine((value) => value.length <= 50, {
+      message: "Name must have at most 50 characters",
+    }),
   isPrivate: z.boolean().optional(),
 });
