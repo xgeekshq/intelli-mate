deea12d22a0fa47ec88630020277cffab526ad35
romansharapov19 <95644495+romansharapov19@users.noreply.github.com>
2023-09-12 15:42:58 +0100

    feat: handle errors (#178)
    * feat: handle errors
    
    * feat: remove scroll from error component

--- packages/web-ui/app/rooms/error.tsx
+++ packages/web-ui/app/rooms/error.tsx
@@ -3,5 +3,5 @@
 import ErrorLayout from '@/components/error-layout';
 
 export default function Error() {
-  return <ErrorLayout minHeight="min-h-[calc(100%-41px)]" />;
+  return <ErrorLayout minHeight="min-h-[calc(100vh-65px)]" />;
 }
