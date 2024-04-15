cff6620f0795a604e9e7231215f9775ec434a021
Guido Pereira <stereopt@gmail.com>
2024-04-15 15:21:11 +0100

    fix: x-invoke-path not working

--- packages/web-ui/middleware.ts
+++ packages/web-ui/middleware.ts
@@ -1,7 +1,23 @@
+import { NextResponse } from 'next/server';
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
   publicRoutes: ['/', '/admin', /^\/admin\/.*/],
+  beforeAuth(req, evt) {
+    // SOLUTION: https://stackoverflow.com/a/77214970
+    const requestHeaders = new Headers(req.headers);
+
+    const url = new URL(req.url);
+    const pathname = url.pathname;
+
+    requestHeaders.set('url-path', pathname);
+
+    return NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    });
+  },
 });
 
 export const config = {
